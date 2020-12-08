07991dac827b5f3a98be386035699bfd7659c575
boertel <ben@comediadesign.com>
2020-12-08 08:32:26 -0800

    fix import

--- django_meilisearch/utils.py
+++ django_meilisearch/utils.py
@@ -1,8 +1,8 @@
-import django.conf import settings
+from django.conf import settings
 
 
 def get_setting(name, default=None):
     try:
-        return getattr(settings, 'MEILISEARCH_{}'.format(name))
+        return getattr(settings, "MEILISEARCH_{}".format(name))
     except AttributeError:
         return default
